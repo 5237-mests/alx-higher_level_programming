94dafaee92ccf4c999ba4de05ced9fd43a8daa39
5237-mests <msfnmulgeta@gmail.com>
2022-06-09 02:21:38 +0000

    adv

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if not my_list:
+        return 0
+    numerator = 0
+    denomerator = 0
+
+    for tup in my_list:
+        numerator += tup[0] * tup[1]
+        denomerator += tup[1]
+
+    return (numerator / denomerator)
