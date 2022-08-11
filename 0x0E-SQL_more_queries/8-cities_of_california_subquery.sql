5298e664f01e9e821779e7457153d684eabfe4d9
5237-mests <msfnmulgeta@gmail.com>
2022-08-11 14:41:09 +0300

    adv

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,3 +1,7 @@
-HHHHHHHHHHHHHHHHHHHHHHHHHH
-WSLLLLLLLLLLLLLLLLL
-MIC
+-- Script that lists all the cities of California registered in the database
+SELECT id, name -- Query to list all the cities from California
+FROM cities
+WHERE state_id = ( -- Query to get the id of California
+      SELECT id
+      FROM states
+      WHERE name = "California");
