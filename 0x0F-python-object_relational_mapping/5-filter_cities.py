80cb83859efa0c72e406433f027c675926f2ef8d
5237-mests <msfnmulgeta@gmail.com>
2022-09-10 15:05:45 +0300

    all

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Lists all states from the database hbtn_0e_0_usa
+List all cities of a state
 """
 import sys
 import MySQLdb
@@ -10,8 +10,9 @@ if __name__ == '__main__':
                          db=sys.argv[3], port=3306)
 
     cur = db.cursor()
-    cur.execute("SELECT * FROM states;")
+    cur.execute("SELECT cities.id, cities.name, states.name \
+    FROM cities JOIN states ON cities.state_id = states.id \
+    WHERE states.name = '{}';".format(sys.argv[4]))
     states = cur.fetchall()
 
-    for state in states:
-        print(state)
+    print(", ".join([state[1] for state in states]))
